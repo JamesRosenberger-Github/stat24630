ba48a59c961e32de081a6a9a0729d9c79f52ee45
Connor Rust <connorrust@Connors-MacBook-Air.local>
2025-11-15 18:39:35 -0500

    accessed underlying data in matched_fish

--- PreliminaryMatching.R
+++ PreliminaryMatching.R
@@ -1,7 +1,8 @@
 # Following Example 8
 
-library("MatchIt")
+library(MatchIt)
 library(tidyverse)
+library(ggplot2)
 
 fish <- read_csv("nhanes_fish.csv")
 
@@ -14,6 +15,11 @@ fish_clean <- fish %>%
 model <- glm(high_fish ~ ., data = fish_clean, family = "binomial")
 eps <- predict(model, type = "response")
 lps <- predict(model)
+
+temp.data <- data.frame(lps = lps, treated = as.factor(fish_clean$high_fish))
+ggplot(temp.data, aes(x = lps, fill = treated, color = treated)) + 
+  geom_histogram(alpha = 0.5, position = "identity") + 
+  xlab("Linearized propensity score") 
 
 
 m.out0 <- matchit(high_fish ~ gender + age + income + income.missing + race +
@@ -29,4 +35,15 @@ m.out1 <- matchit(high_fish ~ gender + age + income + income.missing + race +
                   estimand = "ATT", 
                   method = "nearest", distance = lps)
 summary(m.out1)
+
+matched_fish <- match_data(m.out1)
+
+matched_model <- glm(high_fish ~ ., data = matched_fish, family = "binomial")
+matched_eps <- predict(matched_model, type = "response")
+matched_lps <- predict(matched_model)
+matched_temp.data <- data.frame(lps = matched_lps, treated = as.factor(matched_fish$high_fish))
+
+ggplot(matched_temp.data, aes(x = matched_lps, fill = treated, color = treated)) + 
+  geom_histogram(alpha = 0.5, position = "identity") + 
+  xlab("Linearized propensity score") 
 
